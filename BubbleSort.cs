dba88d11fc8c34324e1e51649715ce4ae1c4e9ad
himanshu01031997 <hs010397@gmail.com>
2022-10-22 19:15:47 +0530

    add:added bubble sort

---
+++ BubbleSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmProblem
+{
+    public class BubbleSort
+    {
+        public void Bubble()
+        {
+            int[] arr = { 78, 55, 45, 13 };
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("sorted:");
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p + "");
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please enter your choice:\n1.Binary search\n");
+            Console.WriteLine("please enter your choice:\n1.Insertion Sort\n2.Bubble Sort\n");
             int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---------------------------------");
             switch (choice)
             {
                 case 1:
                     Insertion insertion = new Insertion();
                     insertion.InsertionOrder();
+                    break;
+                case 2:
+                    BubbleSort sort = new BubbleSort();
+                    sort.Bubble();
+                    break;
+                    default:
                     break;
             }
         }
