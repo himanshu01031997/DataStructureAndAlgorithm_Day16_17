90a21db9a6f9c15537d9aebf2da816e2a1147283
himanshu01031997 <hs010397@gmail.com>
2022-10-22 19:00:38 +0530

    added:insertion sort

---
+++ Insertion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmProblem
+{
+    public class Insertion
+    {
+        public  void InsertionOrder()
+        {
+            int[] arr = new int[5] { 23, 9, 12, 85, 5 };
+            int n = 5, i, j, val, flag;
+            Console.WriteLine("insertion sort");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        flag = 1;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("sorted");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+
+        }
+    }
+        
+    
+}
+      
+
+
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgoritmProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your choice:\n1.Binary search\n");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Insertion insertion = new Insertion();
+                    insertion.InsertionOrder();
+                    break;
+            }
+        }
+
+
+    }
+}
